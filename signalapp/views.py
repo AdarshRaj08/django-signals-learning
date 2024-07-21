59f8d7089141d98c28cd54331484c4fc74fe67f5
Adarsh Raj <iamadarsh008@gmail.com>
2024-07-21 11:53:03 +0530

    second commit

---
+++ signalapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+# signals are utilities that 
+
+
+# sender 
+# signal
+# receiver
+
+
+# Connecting/Registering Receiver Function - There are two ways you can connect a receiver to a signal
+#   Manual connect Route
+#   Decorator
+
+#  
