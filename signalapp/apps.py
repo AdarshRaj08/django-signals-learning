20818dedfad880e27694e57db60a34c64f4129d5
Adarsh Raj <iamadarsh008@gmail.com>
2024-07-21 12:37:06 +0530

    third

--- signalapp/apps.py
+++ signalapp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SignalappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'signalapp'
+
+    def ready(self):
+        import signalapp.signals
